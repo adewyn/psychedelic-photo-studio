7fd00dddbe1775de7aaa74d93950eb06861baa4a
adewyn <stephenw@myetherwallet.com>
2023-04-03 14:25:00 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = new fabric.Canvas("canvas");
+    const inputImage = document.getElementById("inputImage");
+    const applyEffect = document.getElementById("applyEffect");
+  
+    inputImage.addEventListener("change", function (e) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+  
+      reader.onload = function (f) {
+        const imgObj = new Image();
+        imgObj.src = f.target.result;
+  
+        imgObj.onload = function () {
+          const image = new fabric.Image(imgObj);
+          canvas.setWidth(image.width);
+          canvas.setHeight(image.height);
+          canvas.add(image);
+        };
+      };
+  
+      reader.readAsDataURL(file);
+    });
+  
+    applyEffect.addEventListener("click", function () {
+      const object = canvas.getActiveObject();
+      if (!object) return;
+  
+      object.filters.push(new fabric.Image.filters.Invert());
+      object.applyFilters();
+      canvas.renderAll();
+    });
+  });
+  
